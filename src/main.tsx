34f52ede24dc0d002cfe0c11e3890281e8842446
Henrique Araujo <henrique@Mac-mini-de-Henrique.local>
2026-06-20 15:49:47 -0300

    feat(styles) adicionado arquivos theme.css e global.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import App from '@/App';
+
+import '@/styles/theme.css';
+import '@/styles/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
